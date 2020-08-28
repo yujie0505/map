48f7362852ec2ba1840e914684572b88d4f9585e
yujie <seven8355@gmail.com>
2020-08-28 10:15:14 +0800

    Chore(sys): initialize the system

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  #home
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Home extends Vue {}
+</script>
