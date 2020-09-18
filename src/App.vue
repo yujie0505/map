061f80ef4a3f9f16c8e552badf3f84a179a79a61
yujie <seven8355@gmail.com>
2020-09-18 17:46:25 +0800

    Refactor(src): import component WidgetDrawer in view Home

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
 v-app#app
-  WidgetDrawer
   v-main
     router-view
 </template>
@@ -9,12 +8,6 @@ v-app#app
 import Vue from "vue";
 import Component from "vue-class-component";
 
-import WidgetDrawer from "@/components/WidgetDrawer.vue";
-
-@Component({
-  components: {
-    WidgetDrawer,
-  },
-})
+@Component
 export default class App extends Vue {}
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template lang="pug">
 v-container.fill-height.pa-0(fluid)
-  Map
+  WidgetDrawer
+  Map(@selectGrid="MUTATION_MAP_SET_SELECTED_GRID")
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import { namespace } from "vuex-class";
 
 import Map from "@/components/Map.vue";
+import WidgetDrawer from "@/components/WidgetDrawer.vue";
+import { MUTATION_MAP_SET_SELECTED_GRID } from "@/constants/mutations";
+import { Grid } from "@/types/map";
+
+const MapModule = namespace("map");
 
 @Component({
   components: {
     Map,
+    WidgetDrawer,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  @MapModule.Mutation(MUTATION_MAP_SET_SELECTED_GRID) [MUTATION_MAP_SET_SELECTED_GRID]!: (payload: Grid | null) => void;
+}
 </script>
