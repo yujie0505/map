a031776b13855536034e26b90cdf27367ca98216
yujie <seven8355@gmail.com>
2020-09-23 10:28:36 +0800

    Refactor(src): add component Icon for twicon font

---
+++ src/components/Icon.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+i(:class="icon")
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+@Component
+export default class Icon extends Vue {
+  @Prop(String) readonly icon!: string;
+}
+</script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,21 @@
 import "@mdi/font/css/materialdesignicons.css";
-
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
+import "@/assets/twicon/twicon.css";
+import Icon from "@/components/Icon.vue";
+
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  icons: {
+    values: {
+      product: {
+        component: Icon,
+        props: {
+          name: "product",
+        },
+      },
+    },
+  },
+});
