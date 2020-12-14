4f2c841dc6b4648e70ea18b1c693255ddcb4a61b
yujie <seven8355@gmail.com>
2020-12-14 11:35:12 +0800

    Refactor(src): add route for view Circle

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,17 @@ import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 
 import { ROUTE_NAME_CIRCLE, ROUTE_NAME_GRID } from "@/constants/routes";
+import Circle from "@/views/circle/Index.vue";
 import Grid from "@/views/grid/Index.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
+  {
+    path: "/circle",
+    name: ROUTE_NAME_CIRCLE,
+    component: Circle,
+  },
   {
     path: "/grid",
     name: ROUTE_NAME_GRID,
---
+++ src/views/circle/Index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+v-container.fill-height.pa-0(fluid)
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Index extends Vue {}
+</script>
